54145ad048b43c0d0a15e7302074be7898851432
freem627 <freem627@umn.edu>
2024-03-13 12:55:33 -0500

    Update OPL.java

--- Project1/src/main/java/OPL.java
+++ Project1/src/main/java/OPL.java
@@ -4,16 +4,20 @@ import java.util.HashMap;
 
 public class OPL extends Election{
     /**
-     * TODO
-     * @param fileData
+     * Creates an Object of type OPL, which inherits from Election, this is
+	 * used to run the election of the passed in information from fileData
+     * @param fileData a FileData object containing information from the
+	 * original file passed in
      */
     OPL(FileData fileData){
         this.fileData = fileData;
     }
 
     /**
-     * TODO
-     * @return
+     * Runs an OPL election on the content contained in fileData
+     * @return ResultsData which contains all the information from FileData
+	 * 			as well as all the information from running the Election for
+     * 			an OPL system
      */
     @Override
     public ResultsData runElection() {
