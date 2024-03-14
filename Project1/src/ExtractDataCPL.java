034e1652f209498b37cc849e87686c60f62696ff
freem627 <freem627@umn.edu>
2024-03-14 08:56:50 -0500

    Finished ExtractData and ExtractDataCPL, ExtractDataOPL mostly done

--- Project1/src/ExtractDataCPL.java
+++ Project1/src/ExtractDataCPL.java
@@ -3,12 +3,15 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public class ExtractDataCPL extends ExtractData{
+public class ExtractDataCPL extends ExtractData {
     /**
-     * TODO
-     * @param validFile
-     * @param header
-     * @throws IOException
+     * This creates an object of the ExtractDataCPL class
+     *
+     * @param validFile this the bufferedReader used to parse through the given
+     *                  file
+     * @param header    this is the header depicting which type of election it
+     *                  is. This is found in the Main class and passed in
+     * @throws IOException will happen if there is an error in reading the file
      */
     ExtractDataCPL(BufferedReader validFile, String header) throws IOException {
         this.validFile = validFile;
@@ -16,12 +19,41 @@ public class ExtractDataCPL extends ExtractData{
     }
 
     /**
-     * TODO
-     * @param partyVotes
-     * @param candidateVotes
+     * this will format the ballots to update the number of votes in partyVotes,
+     * candidateVotes in the CPL election type does not get updated since all
+     * candidates will have 0 votes personally
+     *
+     * @param partyVotes     this is an ArrayList<ArrayList<Object>> which contains
+     *                       inner mappings of a party name and the number of corresponding
+     *                       votes
+     * @param candidateVotes this is an ArrayList<ArrayList<Object>> whih contains
+     *                       inner mappings of a candidate name and the number of
+     *                       corresponding votes
      */
     @Override
-    protected void formatBallotInformation(ArrayList<ArrayList<Object>> partyVotes, ArrayList<ArrayList<Object>> candidateVotes) {
+    protected void formatBallotInformation(ArrayList<ArrayList<Object>> partyVotes,
+                                           ArrayList<ArrayList<Object>> candidateVotes) {
+        String line = validFile.readLine();
+        char[] splitLine;
+        int index = -1;
+        int count = 0;
 
+        //Check for the EOF
+        while (line != null) {
+            line.trim();
+            splitLine = line.toCharArray();
+
+            for (int i = 0; i < splitLine.length(); i++) {
+                if (splitLine[i] == '1') {
+                    index = i;
+                }
+
+                count = partyVotes.get(index).get(1);
+                count += 1;
+                partyVotes.get(index).set(1, count);
+            }
+
+            line = validFile.readLine();
+        }
     }
 }
--- Project1/src/ExtractDataOPL.java
+++ Project1/src/ExtractDataOPL.java
@@ -1,24 +1,58 @@
 import java.io.BufferedReader;
 import java.util.ArrayList;
 
-public class ExtractDataOPL extends ExtractData{
+public class ExtractDataOPL extends ExtractData {
     /**
-     * TODO
-     * @param validFile
-     * @param header
+     * This creates an object of the ExtractDataOPL class
+     *
+     * @param validFile this the bufferedReader used to parse through the given
+     *                  file
+     * @param header    this is the header depicting which type of election it
+     *                  is. This is found in the Main class and passed in
+     * @throws IOException will happen if there is an error in reading the file
      */
-    ExtractDataOPL(BufferedReader validFile, String header){
+    ExtractDataOPL(BufferedReader validFile, String header) {
         this.validFile = validFile;
         this.header = header;
     }
 
     /**
-     * TODO
-     * @param partyVotes
-     * @param candidateVotes
+     * this will format the ballots to update the number of votes in partyVotes, and
+     * candidateVotes in the OPL election
+     *
+     * @param partyVotes     this is an ArrayList<ArrayList<Object>> which contains
+     *                       inner mappings of a party name and the number of corresponding
+     *                       votes
+     * @param candidateVotes this is an ArrayList<ArrayList<Object>> whih contains
+     *                       inner mappings of a candidate name and the number of
+     *                       corresponding votes
      */
     @Override
-    protected void formatBallotInformation(ArrayList<ArrayList<Object>> partyVotes, ArrayList<ArrayList<Object>> candidateVotes) {
+    protected void formatBallotInformation(ArrayList<ArrayList<Object>> partyVotes,
+                                           ArrayList<ArrayList<Object>> candidateVotes) {
+        String line = validFile.readLine();
+        char[] splitLine;
+        int index = -1;
+        int count = 0;
 
+        //Check for the EOF
+        while (line != null) {
+            line.trim();
+            splitLine = line.toCharArray();
+
+            for (int i = 0; i < splitLine.length(); i++) {
+                if (splitLine[i] == '1') {
+                    index = i;
+                }
+
+                count = candidateVotesVotes.get(index).get(1);
+                count += 1;
+                candidateVotes.get(index).set(1, count);
+            }
+
+            line = validFile.readLine();
+        }
+
+        //TODO give votes to parties as well
     }
 }
