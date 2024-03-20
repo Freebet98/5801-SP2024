2e458e37a563a5416c2ed66f8b7c63d87b1b1425
freem627 <freem627@umn.edu>
2024-03-20 15:56:55 -0500

    ResultsDataCPL updated

--- Project1/src/main/java/ResultsDataCPL.java
+++ Project1/src/main/java/ResultsDataCPL.java
@@ -6,11 +6,15 @@ import java.util.ArrayList;
 
 public class ResultsDataCPL extends ResultsData {
     /**
-     * TODO
-     * @param seatAllocation
-     * @param remainingVotes
-     * @param partyWinOrder
-     * @param fileDate
+     * Creates an Object of the ResultsDataCPL class
+     * 
+     * This stores all the information after running a CPL Election
+     * @param seatAllocation This is an arraylist representing the seat allocation for each party for both the first
+		and second round of allocation. The array is represented as shown <<Dem, [1,3]>, <Rep, [5,0>>
+     * @param remainingVotes This is an arraylist to represent the remaining votes for each party after the first allocation of
+		votes, this cannot be editted after the first allocation
+     * @param partyWinOrder This is an arraylist representing the final win order of an election, CPL: <<Dem, Jon>, <Rep, Mary>>
+     * @param fileDate FileData object, obtained from extracting information from the file
      */
     ResultsDataCPL(ArrayList<ArrayList<Object>> seatAllocation, ArrayList<ArrayList<Object>> remainingVotes,
                    ArrayList<String> partyWinOrder, FileData fileDate ){
