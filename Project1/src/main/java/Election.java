6bdabeb7c454a911ed08cc61fad09b8f8df39f36
DerrickDisch <derrick.dish@gmail.com>
2024-03-20 16:09:59 -0500

    changes made to Election.java

--- Project1/src/main/java/Election.java
+++ Project1/src/main/java/Election.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Random;
 
 abstract public class Election {
     protected FileData fileData;
@@ -54,33 +55,134 @@ abstract public class Election {
     }
 
     /**
-     * Derrick did this
+     * TODO
      * @return
      */
     protected float generateRandom(){
-        return 0;
+        Random rand = new Random();
+        // generates 100 random floats before the retval is calculated to eliminate some psuedo randomness
+        for(int i=0; i<=1000; i++){
+            rand.nextFloat();
+        }
+        // calculates a random float and makes in a range of
+        float retVal = rand.nextFloat()*10;
+        return retVal;
     }
 
     /**
      * TODO
      */
     protected void firstAllocation(){
-
+        int i = 0;
+        int underRemain = 0;
+        // run until there are no more seats to allocate or all parties are under the remainder
+        while(true){
+            int votes = (int) this.remainingVotes.get(i).get(1);
+            if(votes>=largestRemainder){
+                adjustRemainingVotes(i);
+                adjustSeatAllocation(i);
+                addWinner(i);
+                availableSeats--; // a winner was added so a seat should be removed
+            }else{
+                underRemain++;   // what is this supposed to be
+            }
+            i++;
+            if(availableSeats<=0){ // there are no seats, break from loop
+                break;
+            }
+            if(underRemain >= remainingVotes.size()){  // Are all of the parties under the remainder
+                break;                                 // move to second allocation
+            }
+            if(i>=remainingVotes.size()){   // start round robin over
+                underRemain = 0;
+                i = 0;
+            }
+        }
     }
 
     /**
-     * TODO
+     * Preforms the second round of allocation.
+     * Iterates through a list of remaining votes and allocates seats
+     * based on the votes associated with each candidate. 
+     * Resolves ties if multiple candidates have the same highest score.
      */
     protected void secondAllocation(){
+		// creates a new ArrayList<ArrayList<Object>> which is a copy of remainingVote
+        ArrayList<ArrayList<Object>> remainVotesNew = new ArrayList();
+        for(int i =0; i<remainingVotes.size(); i++){
+            ArrayList<Object> innerList = new ArrayList<>(remainingVotes.get(i));
+            remainVotesNew.add(innerList);
+        }
+        while(availableSeats>0){
+            int max = (int)remainVotesNew.get(0).get(1);
+            int index = 0;
+            // finds the candidate with the highest number of votes
+            for (int i=1; i < remainingVotes.size(); i++) {
+                int current = (int) remainingVotes.get(i).get(1);
 
+                if (current > max) {
+                    max = current;
+                    index = i;
+                }
+            }
+            // check to see if there are any ties
+            ArrayList<Integer> indexTie = new ArrayList<>();
+            for (int i = 0; i < remainingVotes.size(); i++) {
+                if (i == index)
+                    continue;
+
+                int current = (int) remainingVotes.get(i).get(1);
+                if (current == max) {
+                    indexTie.add(i);
+                }
+            }
+
+            // resolve ties if any
+            if (!indexTie.isEmpty()) {
+                int location = breakTie(indexTie.size());
+                index = indexTie.get(location);
+            }
+
+            // allocate seat to the candidate with the highest score
+            adjustSeatAllocation(index);
+            int value = (int) remainingVotes.get(index).get(1);
+            value -= largestRemainder;
+            remainingVotes.get(index).set(1, value);
+            availableSeats--; // a winner was added so a seat should be removed
+        }
     }
 
     /**
-     * Derrick did this
-     * @param numTie
-     * @return
+     * Breaks the tie based on a number of tied people, if there is a tie in the tie break
+     * it will rerun breakTie. Determines the winner by generating a random number via generateRandom()
+     * method and comparing to numTie random numbers (also generated via generateRandom()). The closest
+     * number to the compare value is the winner
+     * @param numTie the number of tied candidates/parties
+     * @return returns the index of the winner
      */
     protected int breakTie(int numTie){
-        return 0;
+        // the number used to determine the winner
+        float compval = generateRandom();
+        // an array
+        float[] ties = new float[numTie];
+        for(int i=0; i<ties.length; i++){
+            ties[i] = generateRandom();
+        }
+        float curSmallest = Math.abs(compval - ties[0]);
+        int index = 0;
+        boolean isSame = false;
+        for(int i=1; i<ties.length; i++){
+            if(Math.abs(compval - ties[i])<curSmallest){
+                curSmallest = Math.abs(compval - ties[i]);
+                index = i;
+                isSame = false;
+            }else if(Math.abs(compval - ties[i])==curSmallest){
+                isSame = true;
+            }
+        }
+        if(isSame){
+            return breakTie(numTie);
+        }
+        return index;
     }
 }
