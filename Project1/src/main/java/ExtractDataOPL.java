2e9a5a33d444d1aa0191e3fa83fd5a61a8bf2952
Freebet98 <freem627@umn.edu>
2024-03-24 13:51:37 -0500

    Made changes to ResultsDataCPL

--- Project1/src/main/java/ExtractDataOPL.java
+++ Project1/src/main/java/ExtractDataOPL.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 
 /**
@@ -146,7 +145,6 @@ public class ExtractDataOPL extends ExtractData {
                 }
             }
 
-            
         }
 
         // Gathers the votes from candidateVotes and place in partyVotes
