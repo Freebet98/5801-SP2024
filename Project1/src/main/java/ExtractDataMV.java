a6cdfad9524efdf758e6ebf62a0338b4521aeecc
Bethany Freeman <freem627@umn.edu>
2024-04-18 09:49:28 -0500

    ExtractDataMV Framework

---
+++ Project1/src/main/java/ExtractDataMV.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * This class is used to extract data from the file for the MV election
+ * 
+ * @author TODO
+ * @author Bethany Freeman
+ */
+public class ExtractDataMV extends ExtractData {
+    /**
+     * This creates an object of the ExtractDataMV class
+     *
+     * @param validFiles this is the Arrayist of bufferedReader objects so an
+     *                   election can have multiple files
+     * @param header     this is the header depicting which type of election it
+     *                   is. This is found in the Main class and passed in
+     */
+    ExtractDataMV(ArrayList<BufferedReader> validFiles, String header) {
+        this.validFiles = validFiles;
+        this.header = header;
+    }
+
+    /**
+     * this will format the ballots to update the number of votes in partyVotes, and
+     * candidateVotes in the MV election
+     *
+     * @param partyVotes      this is an ArrayList<ArrayList<Object>> which contains
+     *                        inner mappings of a party name and the number of
+     *                        corresponding
+     *                        votes
+     * @param candidateVotes  this is an ArrayList<ArrayList<Object>> whih contains
+     *                        inner mappings of a candidate name and the number of
+     *                        corresponding votes
+     * @param partyCandidates this is a mapping of a party name to an ordered list
+     *                        of their candidates
+     * @throws IOException
+     */
+    @Override
+    protected void formatBallotInformation(ArrayList<ArrayList<Object>> partyVotes,
+            ArrayList<ArrayList<Object>> candidateVotes, HashMap<String, ArrayList<String>> partyCandidates)
+            throws IOException {
+        // TODO
+    }
+}
