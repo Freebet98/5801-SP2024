fe3b7e4a12fe739693079741ba9c628948318ec3
Rock Zgutowicz <rockzgutowicz@gmail.com>
2024-04-21 15:58:03 -0500

    testFormatPartyInformation() and input files

--- Project2/src/test/java/ExtractDataMVTest.java
+++ Project2/src/test/java/ExtractDataMVTest.java
@@ -22,7 +22,7 @@ import java.util.HashMap;
     *                 All test files must be created and properly formatted
 */
 public class ExtractDataMVTest {
-    ExtractDataMV test01;
+    ExtractDataMV test;
     ArrayList<BufferedReader> validFile;
     HashMap<String, ArrayList<String>> partyCandidates;
     ArrayList<ArrayList<Object>> partyVotes;
@@ -30,29 +30,74 @@ public class ExtractDataMVTest {
 
     @Before
     public void setUp() throws IOException {
-        test01 = new ExtractDataMV(validFile, "MV");
+        test = new ExtractDataMV(validFile, "MV");
     }
 
     @Test
     public void testVerifyLineIsDigit() {
         // Test 1.a
         String line = "";
-        assertEquals(false, test01.verifyLineIsDigit(line));
+        assertEquals(false, test.verifyLineIsDigit(line));
 
         // Test 1.b
         line = "1";
-        assertEquals(true, test01.verifyLineIsDigit(line));
+        assertEquals(true, test.verifyLineIsDigit(line));
 
         // Test 1.c
         line = " 1";
-        assertEquals(true, test01.verifyLineIsDigit(line));
+        assertEquals(true, test.verifyLineIsDigit(line));
 
         // Test 1.d
         line = "abc";
-        assertEquals(false, test01.verifyLineIsDigit(line));
+        assertEquals(false, test.verifyLineIsDigit(line));
 
         // Test 1.e
         line = "12b";
-        assertEquals(false, test01.verifyLineIsDigit(line));
+        assertEquals(false, test.verifyLineIsDigit(line));
     }
+
+    @Test
+    public void testFormatPartyInformation() throws IOException {
+
+        //test 2.a
+        partyVotes = new ArrayList<ArrayList<Object>>();
+        candidateVotes = new ArrayList<ArrayList<Object>>();
+        validFile = new ArrayList<BufferedReader>(Arrays.asList(new BufferedReader(new FileReader(new File(
+            "src/test/java/InputFiles/MVPartyInfo01.txt")))));
+        test = new ExtractDataMV(validFile, "MV");
+        test.validFile = validFile.get(0);
+        partyCandidates = test.formatPartyInformation(partyVotes, candidateVotes, false);
+        HashMap<String, ArrayList<String>> expected = new HashMap<String, ArrayList<String>>();
+
+        expected.put("D", new ArrayList<>(Arrays.asList("Pike", "Foster")));
+        expected.put("R", new ArrayList<>(Arrays.asList("Deutsch", "Borg", "Jones")));
+        expected.put("I", new ArrayList<>(Arrays.asList("Smith")));
+
+        assertEquals(expected, partyCandidates);
+
+        // Test 2.b
+        validFile = new ArrayList<BufferedReader>(Arrays.asList(new BufferedReader(new FileReader(new File("src/test/java/InputFiles/MVPartyInfo02.txt")))));
+        test = new ExtractDataMV(validFile, "MV");
+        test.validFile = validFile.get(0);
+        partyCandidates = test.formatPartyInformation(partyVotes, candidateVotes, false);
+        expected = new HashMap<String, ArrayList<String>>();
+        expected.put("D", new ArrayList<>(Arrays.asList("Pike", "Foster")));
+        expected.put("R", new ArrayList<>(Arrays.asList("Deutsch", "Borg", "Jones")));
+        expected.put("", new ArrayList<>(Arrays.asList("Smith")));
+
+        assertEquals(expected, partyCandidates);
+
+        // test 2.c
+        validFile = new ArrayList<BufferedReader>(Arrays.asList(new BufferedReader(new FileReader(new File("src/test/java/InputFiles/MVPartyInfo03.txt")))));
+        test = new ExtractDataMV(validFile, "MV");
+        test.validFile = validFile.get(0);
+        partyCandidates = test.formatPartyInformation(partyVotes, candidateVotes, true);
+        expected = new HashMap<String, ArrayList<String>>();
+        expected.put("D", new ArrayList<>(Arrays.asList("Pike", "Foster")));
+        expected.put("R", new ArrayList<>(Arrays.asList("Deutsch", "Borg", "Jones")));
+        expected.put("I", new ArrayList<>(Arrays.asList("")));
+
+        assertEquals(expected, partyCandidates);
+    }
+
 }
